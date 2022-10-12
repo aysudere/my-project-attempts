cbcc7500c0b33a8fe637c753159733ab0410c15d
aysudere <aysudere98@gmail.com>
2022-10-12 21:57:52 +0300

    while loops example

---
+++ src/WhileLoopsExample.java
@@ -0,0 +1,18 @@
+public class WhileLoopsExample {
+    public static void main(String[] args) {
+
+        //This program will try to print “Hello World” 5 times.
+        int a = 1;
+        while(a <=5) {
+            System.out.println("Hello World");
+            a++;
+        }
+
+        //This program will find the summation of numbers from 1 to 10.
+        int b = 1;
+        while(b <= 10) {
+            System.out.println(b);
+            b++;
+        }
+    }
+}
